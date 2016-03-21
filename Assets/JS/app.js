5c40cefe7fe6c8753e1cdae234c4e0cdba994db3
Greg Turmel <gregturmel@gmail.com>
2016-03-21 18:12:46 -0400

    add html, add pretty much most of the styling that I"m going to do

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -3,8 +3,8 @@ var myApp = angular.module("myApp", ['ngRoute']);
 // myApp.config(function($routeProvider){
 //    $routeProvider
 //       .when('/', {
-//          templateUrl: ''
+//          templateUrl: 'views/list.html'
 //       })
 // })
 
-//I'm going to probably not need routing for this app - I'm just going to pull in partials, but does that need a routing? I don't think so. I think I'll be able to switch around based on click using ng-click, ng-show, ng-hide, and maybe some other things along those lines. 
+//I'm going to probably not need routing for this app - I'm just going to pull in partials, but does that need a routing? I don't think so. I think I'll be able to switch around based on click using ng-click, ng-show, ng-hide, and maybe some other things along those lines.
--- Assets/JS/controllers.js
+++ Assets/JS/controllers.js
@@ -1,4 +1,13 @@
 myApp.controller("GreetingController", ['$scope', function($scope){
-   $scope.greeting = 'hello!';
+   $scope.greeting = 'Hello! Welcome to the most annoying to-do app in the world';
+
+//defining the to-do object and the fields I'll need to create a new todo (this can get simplified into a list later if I need to drop a few fields)
+function todo(name, time, date, details){
+   this.name = name;
+   this.time = time;
+   this.date = date;
+   this.details = details;
+};
+
 
    }]);
