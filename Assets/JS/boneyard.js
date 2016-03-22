dffaeb8448f85997785e7b1726bf405f566588ec
Greg Turmel <gregturmel@gmail.com>
2016-03-21 20:19:27 -0400

    add two way data binding

---
+++ Assets/JS/boneyard.js
@@ -0,0 +1,12 @@
+//defining the to-do object and the fields I'll need to create a new todo (this can get simplified into a list later if I need to drop a few fields)
+
+// function todo(name, time, date, details){
+//    this.name = name;
+//    this.time = time;
+//    this.date = date;
+//    this.details = details;
+// };
+
+   //I need to tie the input feilds into the function thing. so I need an empty array
+
+   // var newToDo = new todo();
--- Assets/JS/controllers.js
+++ Assets/JS/controllers.js
@@ -1,13 +1,27 @@
 myApp.controller("GreetingController", ['$scope', function($scope){
    $scope.greeting = 'Hello! Welcome to the most annoying to-do app in the world';
 
-//defining the to-do object and the fields I'll need to create a new todo (this can get simplified into a list later if I need to drop a few fields)
-function todo(name, time, date, details){
-   this.name = name;
-   this.time = time;
-   this.date = date;
-   this.details = details;
-};
 
+// this might have to go into a new controller, because I like the two way data binding on the new appointment thing. I also realize that this means going to two pages. Or maybe not...maybe just two sections on one page.
 
-   }]);
+$scope.todos = [{
+   name: 'dance party',
+   time: 'all the time',//try to find a filter for this!
+   date: 'today, yesterday, tomorrow',
+   details: 'just dance',
+}, {
+   name: 'woot woot',
+   time: 'now',
+   date: 'now n forever',
+   details: 'plz plz'
+}]
+
+myApp.controller('AddTodo', ['$scope', function($scope){
+   //this.review = {};
+   this.addReview = function(){
+      $scope.todos.push(this.review)//smthng like this.
+   }
+
+}]);
+
+}]);
