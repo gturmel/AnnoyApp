59c2debd09e039c72f7ee2d9b99de710d2b04d8a
Greg Turmel <gregturmel@gmail.com>
2016-03-21 23:01:34 -0400

    add ability to add tasks

--- Assets/JS/controllers.js
+++ Assets/JS/controllers.js
@@ -4,6 +4,8 @@ myApp.controller("GreetingController", ['$scope', function($scope){
 
 // this might have to go into a new controller, because I like the two way data binding on the new appointment thing. I also realize that this means going to two pages. Or maybe not...maybe just two sections on one page.
 
+
+
 $scope.todos = [{
    name: 'dance party',
    time: 'all the time',//try to find a filter for this!
@@ -11,17 +13,27 @@ $scope.todos = [{
    details: 'just dance',
 }, {
    name: 'woot woot',
-   time: 'now',
+   time: 330,
    date: 'now n forever',
    details: 'plz plz'
-}]
+}] //end of todo's array
 
-myApp.controller('AddTodo', ['$scope', function($scope){
-   //this.review = {};
-   this.addReview = function(){
-      $scope.todos.push(this.review)//smthng like this.
+   this.removeTodo = function($index){
+      $scope.todos.slice($index, 1);
    }
 
-}]);
+}]); //end of Greeting Controller
+
+myApp.controller('AddTodo', ['$scope', function($scope){
+
+   this.todo = {};
+
+   this.addTask = function(todo) {
+      $scope.todos.push(this.todo);
+      this.todo = {};
+   };
+
+
+
 
 }]);
