8104c4355be5af78cfc94b78d28b3a958c022080
Greg Turmel <gregturmel@gmail.com>
2016-03-25 14:44:52 -0400

    it works!

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -8,3 +8,4 @@ var myApp = angular.module("myApp", ['ngRoute']);
 // })
 
 //I'm going to probably not need routing for this app - I'm just going to pull in partials, but does that need a routing? I don't think so. I think I'll be able to switch around based on click using ng-click, ng-show, ng-hide, and maybe some other things along those lines.
+//dependency injection...?
--- Assets/JS/controllers.js
+++ Assets/JS/controllers.js
@@ -4,8 +4,6 @@ myApp.controller("GreetingController", ['$scope', function($scope){
 
 // this might have to go into a new controller, because I like the two way data binding on the new appointment thing. I also realize that this means going to two pages. Or maybe not...maybe just two sections on one page.
 
-
-
 $scope.todos = [{
    name: 'dance party',
    time: 'all the time',//try to find a filter for this!
@@ -18,24 +16,14 @@ $scope.todos = [{
    details: 'plz plz'
 }] //end of todo's array
 
-   this.removeTodo = function($index){
-      $scope.todos.slice($index, 1);
-   }
-
-}]); //end of Greeting Controller
-
-//with lots of information pulled from the Code School tutorial.
-
-myApp.controller('AddTodo', ['$scope', function($scope){
-//creates an object
-   this.todo = {};
-
-   this.addTask = function(todo) { //defines function, uses todo as our value
+   $scope.addTask = function(todo) { //defines function, uses todo as our value
       $scope.todos.push(this.todo); //takes our scope array, adds this.todo to it
       this.todo = {}; //empties out this.todo to reset the form.
    };
 
+   $scope.removeTodo = function($index){
+      $scope.todos.splice($index, 1);
+      console.log('woo');
+   }
 
-
-
-}]);
+}]); //end of Greeting Controller
